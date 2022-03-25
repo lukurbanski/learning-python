8e5c1f799010773174754f6efb6fe014b1fd9823
lukurbanski <lukurbanski@users.noreply.github.com>
2022-03-25 15:35:32 +0000

    flask first

--- flask/main.py
+++ flask/main.py
@@ -1,3 +1,14 @@
 #flask notebook
+from flask import Flask
 
-x = 4
+app = Flask(__name__)
+
+@app.route('/<int:number>/')
+def incrementer(number):
+    return "Incremented number is " + str(number+1)
+
+@app.route('/<string:name>/')
+def hello(name):
+    return "Hello " + name
+    
+app.run()
